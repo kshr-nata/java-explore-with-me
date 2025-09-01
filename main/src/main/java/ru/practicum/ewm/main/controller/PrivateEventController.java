2c81d07dd673fc3faaa0cf1450cc76ed0fc5d3b9
Natalya Abramenko <natalya-gospodinova@yandex.ru>
2025-09-01 22:11:32 +0300

    Этап 2

--- main/src/main/java/ru/practicum/ewm/main/controller/PrivateEventController.java
+++ main/src/main/java/ru/practicum/ewm/main/controller/PrivateEventController.java
@@ -1,7 +1,6 @@
 package ru.practicum.ewm.main.controller;
 
 import jakarta.validation.Valid;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
