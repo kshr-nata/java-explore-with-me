e285a9d37bb6961704dcc193ab5de6a795688fa9
Natalya Abramenko <natalya-gospodinova@yandex.ru>
2025-09-05 22:22:15 +0300

    Этап 2

--- main/src/main/java/ru/practicum/ewm/main/service/EventService.java
+++ main/src/main/java/ru/practicum/ewm/main/service/EventService.java
@@ -389,7 +389,7 @@ public class EventService {
         // Запрашиваем статистику
         ViewStatsRequest statsRequest = ViewStatsRequest.builder()
                 .app("ewm-main-service") // ваше название приложения
-                .start(earliestPublishedDate) // за последний год
+                .start(LocalDateTime.now()) // за последний год
                 .end(LocalDateTime.now())
                 .uris(uris)
                 .unique(false) // все просмотры, а не уникальные
